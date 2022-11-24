88597e2da34b591c45d8011513355396bd0773cd
achica10 <112531727+achica10@users.noreply.github.com>
2022-11-24 00:35:10 -0600

    added six flags great america marker  (with title and animation)

--- js/script.js
+++ js/script.js
@@ -1,10 +1,16 @@
 function initMap() {
-  var chicago = { lat:41.8781, lng:-87.6298};
+  var usa = { lat:37.0902, lng:-95.7129};
   var map = new google.maps.Map(document.getElementById("map"), {
-    center: chicago,
-	zoom: 8,
+    center: usa,
+	zoom: 5,
   });
 
+  new google.maps.Marker({
+    position: new google.maps.LatLng(42.3706,-87.9360),
+    map,
+    label: "Six Flags Great America",
+	animation: google.maps.Animation.DROP
+  });
 
 }
 
